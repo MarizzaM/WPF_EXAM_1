9459e019a8d76e32a5c0966774f214f5c1b17652
Pavlova Marina <marizza.mil89@gmail.com>
2021-04-28 23:58:30 +0300

    Add project files.

---
+++ WPF_EXAM_1/DescriptionWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_EXAM_1
+{
+    /// <summary>
+    /// Interaction logic for DescriptionWindow.xaml
+    /// </summary>
+    public partial class DescriptionWindow : Window
+    {
+        private MainWindow mw;
+        public DescriptionWindow(MainWindow mw)
+        {
+           
+            InitializeComponent();
+            this.DataContext = mw;
+        }
+    }
+}
---
+++ WPF_EXAM_1/LabelConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_EXAM_1
+{
+    class LabelConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double value = System.Convert.ToDouble(values[0]);
+            double maximum = System.Convert.ToDouble(values[1]);
+            double minimum = System.Convert.ToDouble(values[2]);
+
+            double distanceFromMin = value - minimum;
+            double sliderSize = maximum - minimum;
+
+            if ((distanceFromMin / sliderSize * 100) < 25)
+                return "SMALL";
+            if ((distanceFromMin / sliderSize * 100) < 50)
+                return "MEDIUM";
+            if ((distanceFromMin / sliderSize * 100) < 75)
+                return "LARGE";
+            else 
+                return "EXTRA LARGE";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
